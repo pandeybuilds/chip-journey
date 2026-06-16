022d2ae705b331f2005877b8600c6ea6a9c73e8f
Ashutosh Pandey <pandeybuilds@gmail.com>
2026-06-15 17:24:22 -0700

    First variable with number

--- L0-foundations/week01-02-number-boolean/code/base_convert.c
+++ L0-foundations/week01-02-number-boolean/code/base_convert.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("base_convert is alive\n");
+int main (void) {
+    int apple = 42;
+    printf("The number to be printed is %d\n", apple);
     return 0;
 }
